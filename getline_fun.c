ecc2b55b8ebb665c653c9f5209e9b066b90a1445
Fenet-damena <fenetdamena74@gmail.com>
2023-12-09 10:58:14 -0800

    fenet_gelila

--- getline_fun.c
+++ getline_fun.c
@@ -1,62 +1,48 @@
 #include "main.h"
-#define DELIM " \t\r\n\a"
-/
-#include "main.h"
-#define DELIM " \t\r\n\a"
-/
- * getline_fun - function that parses input from the user
- * Return: nothing
- */
-void getline_fun(int argc, char *argv[])
-{
-    char *lineptr = NULL, *token, *args[64];
-    size_t n = 0;
-    int i;
-    if (argc > 1)
-    {
-      for (i = 1; i < argc; i++)
-      {
-        args[i - 1] = argv[i];
-      }
-      args[argc-1] = NULL;
-      ex_command(args);
-      return;
-    }
-    display_prompt();
 
-    while (1)
+/**
+ * read_input - stores whatever is passed to it as standard input
+ *
+ * Return: string containing the input
+ */
+char *read_input(void)
+{
+  char *line = NULL;
+  ssize_t bytes_read;
+  size_t bufsize;
+  int i;
+
+  bufsize = 0;
+  bytes_read = getline(&line, &bufsize, stdin);
+  if (!line)
+  {
+    perror("Error allocating memory for buffer");
+    return (0);
+  }
+  if (bytes_read == 1)
+  {
+    free(line);
+    return (NULL);
+  }
+  else if (bytes_read == EOF)
+  {
+    if (isatty(STDIN_FILENO))
+      write(STDOUT_FILENO, "\n", 1);
+    free(line);
+    exit(0);
+  }
+  else
+  {
+    for (i = 0; line[i] == ' ' && line[i + 1] == ' '; i++)
+      ;
+    if (!line[i] && line[i + 1] == '\n')
     {
-        ssize_t read_size = getline(&lineptr, &n, stdin);
-        if (read_size == -1)
-        {
-            if (lineptr != NULL)
-            {
-                free(lineptr);
-            }
-            break; 
-        }
-        if (read_size > 0)
-        {
-            if (lineptr[read_size - 1] == '\n')
-            {
-                lineptr[read_size - 1] = '\0';
-            }
-            token = my_strtok(lineptr, DELIM);
-            i = 0;
-            while (token != NULL)
-            {
-                args[i] = token;
-                i++;
-                token = my_strtok(NULL, DELIM);
-            }
-            args[i] = NULL;
-      ex_command(args);
-        }
-        display_prompt();
+      free(line);
+      return (0);
     }
-    if (lineptr != NULL)
-    {
-        free(lineptr);
-    }
+  }
+
+  return (line);
 }
+
 
