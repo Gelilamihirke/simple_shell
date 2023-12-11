cf4622e10ba84dd985ecbdc327a9509f268d7956
Gelilamihirke <143137390+Gelilamihirke@users.noreply.github.com>
2023-12-11 21:35:45 +0300

    Update fenet.c

--- fenet.c
+++ fenet.c
@@ -21,12 +21,10 @@ int _atoi(char *s)
 {
 	int i, sign = 1, flag = 0, output;
 	unsigned int result = 0;
-
 	for (i = 0;  s[i] != '\0' && flag != 2; i++)
 	{
 		if (s[i] == '-')
 			sign *= -1;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			flag = 1;
@@ -36,7 +34,6 @@ int _atoi(char *s)
 		else if (flag == 1)
 			flag = 2;
 	}
-
 	if (sign == -1)
 		output = -result;
 	else
@@ -44,12 +41,89 @@ int _atoi(char *s)
 
 	return (output);
 }
+int _myexit(info_t *info)
+{
+	int exitcheck;
 
+	if (info->argv[1])
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+int _mycd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret =
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret =
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+int _myhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array);
+	return (0);
+}
+int _myhistory(info_t *info)
+{
+	print_list(info->history);
+	return (0);
+}
 int unset_alias(info_t *info, char *str)
 {
 	char *p, c;
 	int ret;
-
 	p = _strchr(str, '=');
 	if (!p)
 		return (1);
@@ -63,20 +137,17 @@ int unset_alias(info_t *info, char *str)
 int set_alias(info_t *info, char *str)
 {
 	char *p;
-
 	p = _strchr(str, '=');
 	if (!p)
 		return (1);
 	if (!*++p)
 		return (unset_alias(info, str));
-
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
 }
 int print_alias(list_t *node)
 {
 	char *p = NULL, *a = NULL;
-
 	if (node)
 	{
 		p = _strchr(node->str, '=');
@@ -94,7 +165,6 @@ int _myalias(info_t *info)
 	int i = 0;
 	char *p = NULL;
 	list_t *node = NULL;
-
 	if (info->argc == 1)
 	{
 		node = info->alias;
@@ -113,6 +183,5 @@ int _myalias(info_t *info)
 		else
 			print_alias(node_starts_with(info->alias, info->argv[i], '='));
 	}
-
 	return (0);
 }
