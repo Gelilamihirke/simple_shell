2571ce42e5c4f48d01f216e8a1fd52729effa006
Gelilamihirke <143137390+Gelilamihirke@users.noreply.github.com>
2023-12-11 22:02:31 +0300

    Update abc.c

--- abc.c
+++ abc.c
@@ -78,30 +78,7 @@ int hsh(info_t *info, char **av)
 	}
 	return (builtin_ret);
 }
-int find_builtin(info_t *info)
-{
-	int i, built_in_ret = -1;
-	builtin_table builtintbl[] = {
-		{"exit", _myexit},
-		{"env", _myenv},
-		{"help", _myhelp},
-		{"history", _myhistory},
-		{"setenv", _mysetenv},
-		{"unsetenv", _myunsetenv},
-		{"cd", _mycd},
-		{"alias", _myalias},
-		{NULL, NULL}
-	};
 
-	for (i = 0; builtintbl[i].type; i++)
-		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
-		{
-			info->line_count++;
-			built_in_ret = builtintbl[i].func(info);
-			break;
-		}
-	return (built_in_ret);
-}
 
 
 void find_cmd(info_t *info)
