34088b8bdc39932129ff065f737da7433f7de33d
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2023-12-13 21:05:58 +0300

    Update maingirls1.c

--- main.c
+++ main.c
@@ -1,37 +1,37 @@
 #include "shell.h"
-int main(int ac, char **av)
+int main(int bas, char **mid)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+	info_t idea[] = { INFO_INIT };
+	int kuter = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+		: "=r" (kuter)
+		: "r" (kuter));
 
-	if (ac == 2)
+	if (bas == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		kuter = open(mid[1], O_RDONLY);
+		if (kuter == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(mid[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(mid[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		idea->readfd = kuter;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	populate_env_list(idea);
+	read_history(idea);
+	hsh(idea, mid);
 
 	return (EXIT_SUCCESS);
 }
