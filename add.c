47a4423c8bad30484b012f5bf7342a9d05957b38
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2023-12-13 22:41:02 +0300

    Update add.girls1c

--- add.c
+++ add.c
@@ -1,110 +1,109 @@
 #include "shell.h"
-ssize_t input_buf(info_t *info, char **buf, size_t *len)
+ssize_t input_buf(info_t *idea, char **ebab, size_t *erzemet)
 {
-	ssize_t r = 0;
-	size_t len_p = 0;
-	if (!*len) 
+	ssize_t rage= 0;
+	size_t dherina= 0;
+	if (!*erzemet) 
 	{
-		free(*buf);
-		*buf = NULL;
+		free(*ebab);
+		*ebab= NULL;
 		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-		r = getline(buf, &len_p, stdin);
+		rage= getline(ebab, &dherina, stdin);
 #else
-		r = _getline(info, buf, &len_p);
+		rage = _getline(idea, ebab, &dherina);
 #endif
-		if (r > 0)
+		if (rage > 0)
 		{
-			if ((*buf)[r - 1] == '\n')
+			if ((*ebab)[rage - 1] == '\n')
 			{
-				(*buf)[r - 1] = '\0';
-				r--;
+				(*ebab)[rage - 1] = '\0';
+				rage--;
 			}
-			info->linecount_flag = 1;
-			remove_comments(*buf);
-			build_history_list(info, *buf, info->histcount++);
+			idea->linecount_flag = 1;
+			remove_comments(*ebab);
+			build_history_list(idea, *ebab, idea->histcount++);
 			{
-				*len = r;
-				info->cmd_buf = buf;
+				*erzemet = rage;
+				idea->cmd_buf = ebab;
 			}
 		}
 	}
-	return (r);
+	return (rage);
 }
-ssize_t get_input(info_t *info)
+ssize_t get_input(info_t *idea)
 {
-	static char *buf; 
-	static size_t i, j, len;
-	ssize_t r = 0;
-	char **buf_p = &(info->arg), *p;
+	static char *ebab; 
+	static size_t i, j, erzemet;
+	ssize_t rage = 0;
+	char **ebabg = &(idea->arg), *gado;
 	_putchar(BUF_FLUSH);
-	r = input_buf(info, &buf, &len);
-	if (r == -1)
+	rage = input_buf(idea, &ebab, &erzemet);
+	if (rage == -1)
 		return (-1);
-	if (len)
+	if (erzmet)
 	{
 		j = i;
-		p = buf + i; 
-		check_chain(info, buf, &j, i, len);
-		while (j < len) 
+		gado= ebab + i; 
+		check_chain(idea,ebab, &j, i, erzemet);
+		while (j < erzemet) 
 		{
-			if (is_chain(info, buf, &j))
+			if (is_chain(idea, ebab, &j))
 				break;
 			j++;
 		}
 		i = j + 1;
-		if (i >= len)
-		{
-			i = len = 0;
-			info->cmd_buf_type = CMD_NORM;
+		if (i >= erzemet)
+		{i = erzemet = 0;
+			idea->cmd_buf_type = CMD_NORM;
 		}
-		*buf_p = p; 
-		return (_strlen(p));
+		*ebabg = gado; 
+		return (_strlen(gado));
 	}
-	*buf_p = buf;
-	return (r); 
+	*ebabg= ebab;
+	return (rage); 
 }
-ssize_t read_buf(info_t *info, char *buf, size_t *i)
+ssize_t read_buf(info_t *idea, char *ebab, size_t *i)
 {
-	ssize_t r = 0;
+	ssize_t rage= 0;
 	if (*i)
 		return (0);
-	r = read(info->readfd, buf, READ_BUF_SIZE);
-	if (r >= 0)
-		*i = r;
-	return (r);
+	rage = read(idea->readfd, ebab, READ_BUF_SIZE);
+	if (rage >= 0)
+		*i = rage;
+	return (rage);
 }
-int _getline(info_t *info, char **ptr, size_t *length)
+int _getline(info_t *idea, char **pointer, size_t *kumet)
 {
-	static char buf[READ_BUF_SIZE];
-	static size_t i, len;
+	static char ebab[READ_BUF_SIZE];
+	static size_t i, erzemet;
 	size_t k;
-	ssize_t r = 0, s = 0;
-	char *p = NULL, *new_p = NULL, *c;
-	p = *ptr;
-	if (p && length)
-		s = *length;
-	if (i == len)
-		i = len = 0;
-	r = read_buf(info, buf, &len);
-	if (r == -1 || (r == 0 && len == 0))
+	ssize_t rage = 0, fafi = 0;
+	char *gado = NULL, *addisp= NULL, *wusha;
+	gado = *pointer;
+	if (gado && kumet)
+		fafi = *kumet;
+	if (i == erzemet)
+		i = erzemet= 0;
+	rage = read_buf(idea, ebab, &erzemet);
+	if (rage == -1 || (rage == 0 && erzemet == 0))
 		return (-1);
-	c = _strchr(buf + i, '\n');
-	k = c ? 1 + (unsigned int)(c - buf) : len;
-	new_p = _realloc(p, s, s ? s + k : k + 1);
-	if (!new_p)
-		return (p ? free(p), -1 : -1);
+	wusha = _strchr(ebab + i, '\n');
+	k = wusha ? 1 + (unsigned int)(wusha - ebab) : erzemet;
+	new_p = _realloc(gado, fafi, fafi ? fafi + k : k + 1);
+	if (!addisp)
+		return (gado ? free(gado), -1 : -1);
 	if (s)
-		_strncat(new_p, buf + i, k - i);
+		_strncat(addisp, ebab + i, k - i);
 	else
-		_strncpy(new_p, buf + i, k - i + 1);
-	s += k - i;
+		_strncpy(addisp, ebab + i, k - i + 1);
+	fafi += k - i;
 	i = k;
-	p = new_p;
-	if (length)
-		*length = s;
-	*ptr = p;
-	return (s);
+	gado= addisp;
+	if (kumet)
+		*kumet = fafi;
+	*pointer = gado;
+	return (gado);
 }
 void sigintHandler(__attribute__((unused))int sig_num)
 {
@@ -112,224 +111,224 @@ void sigintHandler(__attribute__((unused))int sig_num)
 	_puts("$ ");
 	_putchar(BUF_FLUSH);
 }
-char *get_history_file(info_t *info)
+char *get_history_file(info_t *idea)
 {
-	char *buf, *dir;
+	char *ebab, *kosh;
 
-	dir = _getenv(info, "HOME=");
-	if (!dir)
+	kosh = _getenv(idea, "HOME=");
+	if (!kosh)
 		return (NULL);
-	buf = malloc(sizeof(char) * (_strlen(dir) + _strlen(HIST_FILE) + 2));
-	if (!buf)
+	ebab= malloc(sizeof(char) * (_strlen(kosh) + _strlen(HIST_FILE) + 2));
+	if (!ebab)
 		return (NULL);
-	buf[0] = 0;
-	_strcpy(buf, dir);
-	_strcat(buf, "/");
-	_strcat(buf, HIST_FILE);
-	return (buf);
+	ebab[0] = 0;
+	_strcpy(ebab, kosh);
+	_strcat(ebab, "/");
+	_strcat(ebab, HIST_FILE);
+	return (ebab);
 }
 
-int write_history(info_t *info)
+int write_history(info_t *idea)
 {
-	ssize_t fd;
-	char *filename = get_history_file(info);
-	list_t *node = NULL;
+	ssize_t kuter;
+	char *semefile = get_history_file(idea);
+	list_t *neteb = NULL;
 
-	if (!filename)
+	if (!semefile)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0644);
-	free(filename);
-	if (fd == -1)
+	kuter= open(semefile, O_CREAT | O_TRUNC | O_RDWR, 0644);
+	free(semefile);
+	if (kuter== -1)
 		return (-1);
-	for (node = info->history; node; node = node->next)
+	for (neteb = idea->history; neteb; neteb = neteb->next)
 	{
-		_putsfd(node->str, fd);
-		_putfd('\n', fd);
+		_putsfd(neteb->str, kuter);
+		_putfd('\n', kuter);
 	}
-	_putfd(BUF_FLUSH, fd);
-	close(fd);
+	_putfd(BUF_FLUSH,kuter);
+	close(kuter);
 	return (1);
 }
 
 
-int read_history(info_t *info)
+int read_history(info_t *idea)
 {
-	int i, last = 0, linecount = 0;
-	ssize_t fd, rdlen, fsize = 0;
-	struct stat st;
-	char *buf = NULL, *filename = get_history_file(info);
+	int i, dhuma = 0, mesmerk = 0;
+	ssize_t kuter, gel, kilo= 0;
+	struct stat meri;
+	char *ebab = NULL, *semefile = get_history_file(idea);
 
-	if (!filename)
+	if (!semefile)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	free(filename);
-	if (fd == -1)
+	kuter= open(semefile, O_RDONLY);
+	free(semefile);
+	if (kuter== -1)
 		return (0);
-	if (!fstat(fd, &st))
-		fsize = st.st_size;
-	if (fsize < 2)
+	if (!fstat(kuter, &meri))
+		kilo = meri.st_size;
+	if (kilo < 2)
 		return (0);
-	buf = malloc(sizeof(char) * (fsize + 1));
-	if (!buf)
+	ebab= malloc(sizeof(char) * (kilo + 1));
+	if (!ebab)
 		return (0);
-	rdlen = read(fd, buf, fsize);
-	buf[fsize] = 0;
-	if (rdlen <= 0)
-		return (free(buf), 0);
-	close(fd);
-	for (i = 0; i < fsize; i++)
-		if (buf[i] == '\n')
+	gel = read(kuter, ebab, kilo);
+	ebab[kilo] = 0;
+	if (gel <= 0)
+		return (free(ebab), 0);
+	close(kuter);
+	for (i = 0; i < kilo; i++)
+		if (ebab[i] == '\n')
 		{
-			buf[i] = 0;
-			build_history_list(info, buf + last, linecount++);
-			last = i + 1;
+			ebab[i] = 0;
+			build_history_list(idea,ebab + dhuma,mesmerk++);
+			dhuma = i + 1;
 		}
-	if (last != i)
-		build_history_list(info, buf + last, linecount++);
-	free(buf);
-	info->histcount = linecount;
-	while (info->histcount-- >= HIST_MAX)
-		delete_node_at_index(&(info->history), 0);
-	renumber_history(info);
-	return (info->histcount);
+	if (dhumat != i)
+		build_history_list(idea, ebab + dhuma, mesmerk++);
+	free(ebab);
+	idea->histcount = mesmerk;
+	while (idea->histcount-- >= HIST_MAX)
+		delete_node_at_index(&(idea>history), 0);
+	renumber_history(idea);
+	return (idea->histcount);
 }
 
 
-int build_history_list(info_t *info, char *buf, int linecount)
+int build_history_list(info_t *idea, char *ebab, int mesmerk)
 {
-	list_t *node = NULL;
+	list_t *neteb = NULL;
 
-	if (info->history)
-		node = info->history;
-	add_node_end(&node, buf, linecount);
+	if (idea->history)
+		neteb = idea->history;
+	add_node_end(&neteb, ebab, mesmerk);
 
-	if (!info->history)
-		info->history = node;
+	if (!idea>history)
+		idea->history = neteb;
 	return (0);
 }
 
-int renumber_history(info_t *info)
+int renumber_history(info_t *idea)
 {
-	list_t *node = info->history;
+	list_t *neteb = idea->history;
 	int i = 0;
 
-	while (node)
+	while (neteb)
 	{
-		node->num = i++;
-		node = node->next;
+		neteb->num = i++;
+		neteb = neteb->next;
 	}
-	return (info->histcount = i);
+	return (idea->histcount = i);
 }
-list_t *add_node(list_t **head, const char *str, int num)
+list_t *add_node(list_t **mata, const char *were, int num)
 {
 	list_t *new_head;
-	if (!head)
+	if (!mata)
 		return (NULL);
 	new_head = malloc(sizeof(list_t));
 	if (!new_head)
 		return (NULL);
 	_memset((void *)new_head, 0, sizeof(list_t));
 	new_head->num = num;
-	if (str)
+	if were)
 	{
-		new_head->str = _strdup(str);
-		if (!new_head->str)
+		new_head->were = _strdup(were);
+		if (!new_head->were)
 		{
 			free(new_head);
 			return (NULL);
 		}
 	}
-	new_head->next = *head;
-	*head = new_head;
+	new_head->next = *mata;
+	*mata= new_head;
 	return (new_head);
 }
-list_t *add_node_end(list_t **head, const char *str, int num)
+list_t *add_node_end(list_t **mata, const char *were, int num)
 {
-	list_t *new_node, *node;
-	if (!head)
+	list_t *new_node, *neteb;
+	if (!mata)
 		return (NULL);
-	node = *head;
+	neteb = *mata;
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 	_memset((void *)new_node, 0, sizeof(list_t));
 	new_node->num = num;
-	if (str)
+	if (were)
 	{
-		new_node->str = _strdup(str);
-		if (!new_node->str)
+		new_node->were = _strdup(were);
+		if (!new_node->were)
 		{
 			free(new_node);
 			return (NULL);
 		}
 	}
-	if (node)
+	if (neteb)
 	{
-		while (node->next)
-			node = node->next;
-		node->next = new_node;
+		while (neteb->next)
+			neteb = neteb->next;
+		neteb->next = new_node;
 	}
 	else
-		*head = new_node;
+		*mata = new_node;
 	return (new_node);
 }
-size_t print_list_str(const list_t *h)
+size_t print_list_str(const list_t *yero)
 {
 	size_t i = 0;
-	while (h)
+	while (yero)
 	{
-		_puts(h->str ? h->str : "(nil)");
+		_puts(h->were ? yero->were : "(nil)");
 		_puts("\n");
-		h = h->next;
+		yero = yero->next;
 		i++;
 	}
 	return (i);
 }
-int delete_node_at_index(list_t **head, unsigned int index)
+int delete_node_at_index(list_t **mata, unsigned int tekoma)
 {
-	list_t *node, *prev_node;
+	list_t *neteb, *prev_node;
 	unsigned int i = 0;
-	if (!head || !*head)
+	if (!mata || !*mata)
 		return (0);
-	if (!index)
+	if (!tekoma)
 	{
-		node = *head;
-		*head = (*head)->next;
-		free(node->str);
-		free(node);
+		neteb= *mata;
+		*mata= (*mata)->next;
+		free(neteb->were);
+		free(neteb);
 		return (1);
 	}
-	node = *head;
-	while (node)
+	neteb = *mata;
+	while (neteb)
 	{
-		if (i == index)
+		if (i == tekoma)
 		{
-			prev_node->next = node->next;
-			free(node->str);
-			free(node);
+			prev_node->next = neteb->next;
+			free(neteb->were);
+			free(neteb);
 			return (1);
 		}
 		i++;
-		prev_node = node;
-		node = node->next;
+		prev_node = neteb;
+		neteb = neteb->next;
 	}
 	return (0);
 }
 void free_list(list_t **head_ptr)
 {
-	list_t *node, *next_node, *head;
+	list_t *neteb, *next_node, *mata;
 	if (!head_ptr || !*head_ptr)
 		return;
-	head = *head_ptr;
-	node = head;
-	while (node)
+	mata = *head_ptr;
+	neteb = mata;
+	while (neteb)
 	{
-		next_node = node->next;
-		free(node->str);
-		free(node);
-		node = next_node;
+		next_node = neteb->next;
+		free(neteb->were);
+		free(neteb);
+		neteb = next_node;
 	}
 	*head_ptr = NULL;
 }
