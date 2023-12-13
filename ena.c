4877b8076094e82b5537340c42285af68b50f1b3
Fenet-damena <138729138+Fenet-damena@users.noreply.github.com>
2023-12-13 23:51:30 +0300

    Update ena.c

--- ena.c
+++ ena.c
@@ -11,7 +11,7 @@ char *_getenv(info_t *info, const char *name)
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
+		p = starts_with(node->were, name);
 		if (p && *p)
 			return (p);
 		node = node->next;
